268d1952e31b767a777db0bb04e8145544f2d6fd
April Peng <helloaprilpeng@gmail.com>
2016-07-11 19:26:51 -0700

    fix bugs in addToTotal to update total amount from zero. create hold function with output that adds to total and resets array

--- js/index.js
+++ js/index.js
@@ -3,27 +3,35 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function Player(rollTotal) {
-  this.total = rollTotal;
+function Player() {
+  this.total = 0;
   this.rolls = [];
 }
 
 // use when player wants to hold
 Player.prototype.addToTotal = function() {
-  return this.rolls.reduce(function(a,b) {
-    return a + b;
-  }) + this.total;
+   return (this.rolls.reduce(function(a,b) {
+     return a + b;
+   }) + this.total);
 }
 
-function Computer(rollTotal) {
-  this.total = rollTotal;
+function Computer() {
+  this.total = 0;
 }
 
 //user interface logic
 $(function() {
-  var newPlayer = new Player(0);
+  var newPlayer = new Player();
   $("#dice").click(function() {
-    $("#output").text(getRandom(1, 6));
-    newPlayer.rolls.push(parseInt($("#output").text()));
-  })
+    $("#diceOutput").text(getRandom(1, 6));
+    newPlayer.rolls.push(parseInt($("#diceOutput").text()));
+    console.log(newPlayer.rolls);
+  });
+
+  $("#hold").click(function() {
+    newPlayer.total = newPlayer.addToTotal();
+    console.log(newPlayer.total);
+    newPlayer.rolls = [];
+    $("#totalOutput").text(newPlayer.total);
+  });
 });
